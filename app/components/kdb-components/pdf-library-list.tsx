8bf567691839566a05b773cfedbcf8ccbfb97c22
Derick80 <derickchoskinson@gmail.com>
2023-06-16 03:55:02 -0500

    testing react-pdf

--- app/components/kdb-components/pdf-library-list.tsx
+++ app/components/kdb-components/pdf-library-list.tsx
@@ -4,7 +4,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import React, { useCallback } from "react";
 import type { loader } from "~/routes/kdb";
 import type { PdfLibrary } from "~/types/kdb-types";
-// import PdfViewer from "./pdf-viewer";
+import PdfViewer from "./pdf-viewer";
 import EditAbstract from "./pdf-edit-abstract";
 
 export default function PdfList() {
@@ -65,7 +65,7 @@ export default function PdfList() {
                   </div>
                 </li>
               </ul>
-              {/* <PdfViewer pdfUrl={kdbEntry.internalPdfUrl} /> */}
+              <PdfViewer pdfUrl={kdbEntry.internalPdfUrl} />
             </div>
           </AccordianTriggerBar>
         </div>
